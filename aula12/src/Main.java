02e317d6e7b724d100e8540e3ad9a4808628327a
dju4mota <fernando.b@ges.inatel>
2022-10-18 19:17:24 -0300

    Aula 12

--- aula12/src/Main.java
+++ aula12/src/Main.java
@@ -2,44 +2,50 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Objetos para saída de dados
         OutputStream fluxoSaida = null;
         OutputStreamWriter geradorFluxoSaida = null;
         BufferedWriter bufferSaida = null;
 
+        // Objetos para entrada de dados
         InputStream fluxoEntrada = null;
         InputStreamReader geradorFluxoEntrada = null;
         BufferedReader bufferEntrada = null;
 
+//        Colocamos essa parte dentro do try-catch para lidar com as exceções de arquivos
         try {
+//            Inicializando os objetos
             fluxoSaida = new FileOutputStream("scratch.txt",true);
             geradorFluxoSaida = new OutputStreamWriter(fluxoSaida);
             bufferSaida = new BufferedWriter(geradorFluxoSaida);
+
+//            Escrita no arquivo
             bufferSaida.write("POO!");
             bufferSaida.newLine();
 
+//            Inicializando os objetos
             fluxoEntrada = new FileInputStream("scratch.txt");
             geradorFluxoEntrada = new InputStreamReader(fluxoEntrada);
             bufferEntrada = new BufferedReader(geradorFluxoEntrada);
 
+
+//           Leitura no arquivo
             String leitura  = bufferEntrada.readLine();
             System.out.println(leitura);
 
-//            StringBuilder textoLido = new StringBuilder();
-//
-//            while(bufferEntrada.ready()){
-//                textoLido.append(bufferEntrada.readLine() + "\n");
-//            }
-
-//            System.out.println(textoLido.toString().replace("Hello, World!", "!"));
-
         }catch (Exception e){
-            System.out.println(e);
+            System.err.printf(String.valueOf(e));
         } finally {
+//            bloco finally para fechar o buffer independente se teve exception ou não
             try {
+                assert bufferSaida != null;
                 bufferSaida.close();
+                assert bufferEntrada != null;
+                bufferEntrada.close();
                 System.out.println("Sistema Encerrado");
             } catch (IOException e) {
-                System.out.println(e);
+                System.err.printf(String.valueOf(e));
             }
         }
 
